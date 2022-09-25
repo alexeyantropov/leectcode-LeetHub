1dcd7eba9f909ab2bab4e5b9e132c573686d09b2
Alexey <alexeyantropov@gmail.com>
2022-09-25 15:51:45 +0300

    Time: 133 ms (17.17%), Space: 13.4 MB (36.20%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -5,8 +5,8 @@ class Solution(object):
         :rtype: bool
         """
         x_ = str(x)                 # Make the the list iterable.
-        if len(str(x)) < 2:
-            return(True)
+        # The first way.
+        """
         for i in range(0, len(x_)):
             if i >= len(x_)//2:
                 return(True)        # We have checked a half of the list and there is no reason to go further. The solution is there!
@@ -14,3 +14,11 @@ class Solution(object):
                 continue            # The left and the right elements checked. On next step it will move pointers to a next left and righ elements.
             else:
                 return(False)
+        """
+        # The second way! It tries to find different elements and return immediately.
+        for i in range(0, len(x_)):
+            if i >= len(x_)//2:
+                return(True)
+            if x_[i] != x_[-1-i]:
+                return(False)
+        return True
