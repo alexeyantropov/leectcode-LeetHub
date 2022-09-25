13c45ccc8fb54d371362b89a05cbce9ffd55a714
Alexey <alexeyantropov@gmail.com>
2022-09-25 15:10:42 +0300

    Time: 80 ms (70.34%), Space: 14.2 MB (85.63%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -6,7 +6,17 @@ class Solution(object):
         :rtype: List[int]
         """
         # A naive way there. It takes O(N^2) time.
+        """
         for i in range(0, len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return([i,j])
+        """
+        # A true way with extra dict for saving results of prev iterations.   
+        tmp = {}
+        for i in range(0, len(nums)):
+            expected_value = target - nums[i] # 
+            if nums[i] in tmp:
+                return([tmp[nums[i]], i])
+            else:
+                tmp[expected_value] = i
