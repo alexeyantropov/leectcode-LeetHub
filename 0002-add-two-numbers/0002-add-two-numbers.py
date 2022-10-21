b41952ac5af2504b830118a892a33a58f53a719a
Alexey <alexeyantropov@gmail.com>
2022-10-21 22:19:51 +0300

    Time: 101 ms (55.83%), Space: 13.5 MB (42.60%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,23 +10,23 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        head = ListNode('a head', None)
-        ptr = head
+        head = ListNode(0, None)
+        ptr = head                  # Set a pointer to the first element of the list.
         memo = 0
         while l1 or l2 or memo:
             if l1:
                 l1_value = l1.val
-                l1 = l1.next        # Move a pointer of the first linked list.
+                l1 = l1.next        # Try to look at the 'l1' var like a pointer on the first/current node of the 'l1' list. And move it. 
             else:
                 l1_value = 0
                 
             if l2:
                 l2_value = l2.val
-                l2 = l2.next        # Move a second pointer.
+                l2 = l2.next        # Move the second pointer.
             else:
                 l2_value = 0
             
-            # The next code will fix with '//' and '%' operators!!
+            # The next code block could be rewritten with '%' and '//' operators.
             new_node_val = l1_value + l2_value + memo
             if new_node_val > 9:
                 memo = 1
@@ -34,9 +34,9 @@ class Solution(object):
             else:
                 memo = 0
             
-            new_node = ListNode(new_node_val)
-            ptr.next = new_node
-            ptr = new_node
+            new_node = ListNode(new_node_val)   # Set a new element.
+            ptr.next = new_node                 # Link the current element with a new one. In first step head -> new_node.
+            ptr = new_node                      # Move the pointer to the next one element.
             
         return(head.next)           # It returns the output list w/o firs dummy-head node.
             
