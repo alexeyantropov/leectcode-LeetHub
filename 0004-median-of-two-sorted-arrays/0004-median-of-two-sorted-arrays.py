f570cc41f69a8227722a040b50125059ed6dc6b7
Alexey <alexeyantropov@gmail.com>
2022-10-23 17:22:39 +0300

    Time: 186 ms (23.97%), Space: 13.5 MB (92.18%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -6,8 +6,11 @@ class Solution(object):
         :rtype: float
         """
         merged_list = sorted(nums1 + nums2)
-        if len(merged_list)%2: # True if it has a modulo after division -> the list has a middle element.
+        if len(merged_list)%2: # True if expression has a modulo after division -> the list has a middle element.
             median = merged_list[len(merged_list)//2]
         else:
             median = (merged_list[len(merged_list)//2] + merged_list[len(merged_list)//2 - 1]) / 2.0
         return(median)
+    
+        # P.S:
+        # I have seen solutions from the Discuss tab. But(!) I'm sure that lists concatenation and sorting from standard library is much faster and better way!
