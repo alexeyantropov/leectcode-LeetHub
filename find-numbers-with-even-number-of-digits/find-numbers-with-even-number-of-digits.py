ccfd074a380b98d3f86bc2281b3c41a16572109e
Alexey <alexeyantropov@gmail.com>
2023-02-06 17:18:43 +0300

    Time: 47 ms, Memory: 13.8 MB - LeetHub

---
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # The simpest way is len(str)) instead of dividing (//) by 10
+        ret = 0
+        for i in range(len(nums)):
+            tmp = nums[i]
+            counter = 0
+            while (tmp):
+                tmp = tmp//10
+                counter += 1
+            if counter % 2 == 0:
+                ret += 1
+        return(ret)
