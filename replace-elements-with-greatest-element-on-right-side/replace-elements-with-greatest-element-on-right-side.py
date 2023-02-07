12e1be4e78d6cd35f5bb14bccd55347f8f110852
Alexey <alexeyantropov@gmail.com>
2023-02-07 22:40:31 +0300

    Time: 662 ms, Memory: 15.2 MB - LeetHub

--- replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -4,15 +4,21 @@ class Solution(object):
         :type arr: List[int]
         :rtype: List[int]
         """
+        # The vars greatest_element_* follow front Hint #2 
         greatest_element_num = 0
         greatest_element_val = arr[greatest_element_num]  
         for i in range(len(arr)-1):
-            if greatest_element_num <= i:
-                greatest_element_val = arr[i+1]
-                for j in range(i+1, len(arr)):
-                    if arr[j] >= greatest_element_val:
-                        greatest_element_val = arr[j]
-                        greatest_element_num = j
+            # Next condition saves time by reducing O(n-i) extra lookups for the maximum value.
+            # It really usefull when list like [1,2,3,4,<many values>,9999,1,2,3]. The IF condition will skipp these <many values>.
+            if greatest_element_num > i:
+                arr[i] = greatest_element_val
+                continue
+            # If the code have passed through an index of the former maximum value it has to find a new one.
+            greatest_element_val = arr[i+1]
+            for j in range(i+1, len(arr)):
+                if arr[j] >= greatest_element_val:
+                    greatest_element_val = arr[j]
+                    greatest_element_num = j
             arr[i] = greatest_element_val
             
         arr[-1] = -1
