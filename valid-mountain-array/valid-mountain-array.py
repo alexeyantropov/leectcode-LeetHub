d629979d147d8ee13eea7c30658800b665d143f7
Alexey <alexeyantropov@gmail.com>
2023-02-07 19:46:44 +0300

    Time: 161 ms, Memory: 15.1 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.py
+++ valid-mountain-array/valid-mountain-array.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type arr: List[int]
         :rtype: bool
         """
+        # The first solution. It runs on the hill and checks the top is passed or not.
+        # It always consumes O(N) time.
+        """
         is_upward = None
         for i in range(len(arr) - 1):
             if arr[i] < arr[i+1] and is_upward == None:
@@ -18,4 +21,24 @@ class Solution(object):
                 continue
             return(False)
         return(len(arr) > 2 and is_upward == False)
-        
+        """
+        # The second. A try to use two pointers.
+        # It consumes about O(N/2) time and in-loop conditions are easier.
+        l = 0
+        r = len(arr) - 1
+        if len(arr) < 3:
+            return(False)
+        while l < r:
+            l_moved = False
+            r_moved = False
+            if arr[l] < arr[l+1]:
+                l_moved = True
+                l += 1
+            if arr[r] < arr[r-1]:
+                r -= 1
+                l_moved = True
+            if l_moved or r_moved:
+                continue
+            else:
+                return(False)
+        return(l == r and l != 0 and r != len(arr)-1)
