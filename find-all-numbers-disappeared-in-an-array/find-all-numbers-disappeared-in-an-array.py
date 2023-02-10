d687001d5cc044a728f16fe47ceffccb4a15a081
Alexey <alexeyantropov@gmail.com>
2023-02-10 12:15:00 +0300

    Time: 321 ms, Memory: 20.6 MB - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -7,18 +7,24 @@ class Solution(object):
         """
         # The naive solutions is build an empty list and fill it by elements from the 'nums' list.
         # After we could find empty elements indexes and vaules. It needs extra space for a tmp list.
+        # It needs extra memory but doesn't change the 'nums' list.
         
-        # The second solution is the "barley-break" method. But nested loop could take some time.
+        # The second brude-force solution is the "barley-break" method.
+        # But nested loop could take some time to place each element on the plase.
         ret = []
         for i in range(len(nums)):
-            if i != nums[i] - 1: # A value is out of the place. Possible it's a desired value. 
+            # A value is out of the place. Possible it's a desired value. 
+            if i != nums[i] - 1:
                 index_to = nums[i] - 1
                 while True:
                     nums[i], nums[index_to] = nums[index_to], nums[i]
                     index_to = nums[i] - 1
-                    if index_to == nums[index_to] - 1: # A value on the place
+                    # The 'while' loop has reaced the goal if the vaule on the place.
+                    if index_to == nums[index_to] - 1: 
                         break
-            if i != nums[i] - 1: # Double check. A value has been fixed or no after the game.
+            # Double check. The value on the place or no?
+            # On this step we can get expected disappeared values or vaules that haven't fixed yet.
+            if i != nums[i] - 1:
                 ret.append(i+1)
         # Some values have been fixed, some no. Final check here. << O(n)
         ret_ = []
@@ -28,13 +34,15 @@ class Solution(object):
         return(ret_)
         """
         
-        # The third solution - mark values that just could be on the places,
-        # this way we will find indexes w/o the marks.
+        # The third solution - mark/flip values that on the places by '-1'
+        # It is the fastest solution.
         ret = []
         for i in range(len(nums)):
-            print(i)
+            # Mark values on all indexes that is reads from values.
             nums[abs(nums[i]) - 1] = abs(nums[abs(nums[i]) - 1]) * -1
         for i in range(len(nums)):
+            # If a vaules isn't marked - the first loop didn't gen this index 'i'.
+            # And expected disappeared value (or index) here!
             if abs(nums[i]) == nums[i]:
                 ret.append(i+1)
         return(ret)
