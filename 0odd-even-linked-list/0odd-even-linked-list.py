e825ef42f001d5697dbf115241bca88a0c64b811
Alexey <alexeyantropov@gmail.com>
2023-02-17 11:41:31 +0300

    Time: 37 ms, Memory: 17.2 MB - LeetHub

--- 0odd-even-linked-list/0odd-even-linked-list.py
+++ 0odd-even-linked-list/0odd-even-linked-list.py
@@ -9,12 +9,16 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
+        """
         # head = [1,2,3,4,5] -> [2,4] + [1,3,5] -> [2,4,1,3,5]
+        # Main idea is using a flag variable that changes every step.
+        # This way the code can put odd and even variables to different list
         ptr = head
         head_1 = ListNode(None)
         head_2 = ListNode(None)
         ptr_1 = head_1
         ptr_2 = head_2
+        # The flag.
         flag = True
         while ptr:
             if flag:
@@ -28,3 +32,24 @@ class Solution(object):
         ptr_2.next = None
         ptr_1.next = head_2.next
         return(head_1.next)
+        """
+        # The second way w/o flag
+        # The odd-numbered nodes will be connected to each other (from the first node of the 'head' list),
+        # and a tmp list will be buill from the even-numbered nodes.
+        # [1,2,3,4,5] + None -> [1,3,4,5] + [1] -> [1,3,5] + [1,4]
+        if not head:
+            return(None)
+        elif not head.next:
+            return(head)
+        else:
+            pass
+        ptr = head
+        head_2 = head.next
+        ptr_2 = head_2
+        while ptr.next and ptr_2.next:
+            ptr.next = ptr_2.next
+            ptr = ptr_2.next
+            ptr_2.next = ptr.next
+            ptr_2 = ptr_2.next
+        ptr.next = head_2
+        return(head)
