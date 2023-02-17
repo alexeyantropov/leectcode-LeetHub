e325c4e30e2267d096c3efe42bcb2fecab5a911d
Alexey <alexeyantropov@gmail.com>
2023-02-17 19:37:47 +0300

    Time: 112 ms, Memory: 14.4 MB - LeetHub

--- find-pivot-index/find-pivot-index.py
+++ find-pivot-index/find-pivot-index.py
@@ -4,12 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # [1,7,3,6,5,6]
-        #  ^        sum_left=0, sum_right=sum(nums)
-        #    ^      0+1,        sum(nums) - 7
-        #      ^    0+1+7,      sum(nums) - 7 - 6
+        """
         sum_left = 0
         sum_right = sum(nums)
+        # The first. It needs extra conditions.
         for i in range(0,len(nums)):
             if i > 0:
                 sum_left += nums[i-1]
@@ -17,4 +15,14 @@ class Solution(object):
                 sum_right -= nums[i]
             if sum_left == sum_right:
                 return(i)
+        return(-1)
+        """
+        # The second. It's better.
+        sum_left = 0
+        sum_right = sum(nums)
+        for i in range(len(nums)):
+            sum_right -= nums[i]
+            if sum_left == sum_right:
+                return(i)
+            sum_left += nums[i]
         return(-1)
