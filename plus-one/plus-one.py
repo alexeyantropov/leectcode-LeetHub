75a44ff1586bfc34bc80da9cfa205d47a7eabd45
Alexey <alexeyantropov@gmail.com>
2023-02-21 16:11:43 +0300

    Time: 18 ms, Memory: 13.2 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -5,5 +5,20 @@ class Solution(object):
         :rtype: List[int]
         """
         # The naive sol with the standat library.
+        """
         tmp = int(''.join([str(x) for x in digits])) + 1
         return([int(x) for x in str(tmp)])
+        """
+        # True way with iterate through the list with extra space
+        tmp = 0
+        ret = []
+        for i in range(len(digits)):
+            if i == 0:
+                s = digits[-1-i] + 1 + tmp
+            else:
+                s = digits[-1-i] + tmp
+            tmp = s // 10
+            ret.append(s%10)
+        if tmp > 0:
+            ret.append(1)
+        return(ret[::-1])
