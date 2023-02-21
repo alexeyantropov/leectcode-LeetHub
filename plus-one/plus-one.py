e5d79c444d54c2c184015acd9fd1ef63718517f7
Alexey <alexeyantropov@gmail.com>
2023-02-21 16:35:59 +0300

    Time: 21 ms, Memory: 13.5 MB - LeetHub

--- plus-one/plus-one.py
+++ plus-one/plus-one.py
@@ -9,7 +9,8 @@ class Solution(object):
         tmp = int(''.join([str(x) for x in digits])) + 1
         return([int(x) for x in str(tmp)])
         """
-        # True way with iterate through the list with extra space
+        """
+        # A way with iterate through the list with extra space.
         tmp = 0
         ret = []
         for i in range(len(digits)):
@@ -22,3 +23,16 @@ class Solution(object):
         if tmp > 0:
             ret.append(1)
         return(ret[::-1])
+        """
+        # What about avoid to extra space usage?
+        tmp = 0
+        digits = digits[::-1] # [1,2,3] -> [3,2,1]
+        digits[0] = digits[0] + 1
+        for i in range(len(digits)):
+            digits[i] = digits[i] + tmp
+            tmp = digits[i] // 10
+            if tmp > 0:
+                digits[i] = digits[i] % 10
+        if tmp > 0:
+            digits.append(1)
+        return(digits[::-1])
