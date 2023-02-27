ca07c7fedd0d2288947d09301c24e30dae6efc9d
Alexey <alexeyantropov@gmail.com>
2023-02-27 20:38:07 +0300

    Time: 52 ms, Memory: 13.6 MB - LeetHub

--- implement-strstr/implement-strstr.py
+++ implement-strstr/implement-strstr.py
@@ -6,7 +6,31 @@ class Solution(object):
         :rtype: int
         """
         # The first solution. Built-in operators.
+        """
         if needle in haystack:
             return(haystack.index(needle))
         else:
             return(-1)
+        """
+        # The second.
+        # sadbutsad
+        # sad, but, nut, bud
+        i, j, ret = 0, 0, -1
+        while i < len(haystack):
+            print(i,j)
+            if needle[j] == haystack[i]:
+                if ret == -1: # index init
+                    ret = i
+                if j == len(needle) - 1:
+                    return(ret)
+                i += 1
+                j += 1
+            elif ret == -1: 
+                i += 1
+            else:
+                i = ret + 1
+                j = 0
+                ret = -1
+                
+        return(-1)
+                
