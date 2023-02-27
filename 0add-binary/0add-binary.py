5db590760827c5031d5b6820f7343f2980680d85
Alexey <alexeyantropov@gmail.com>
2023-02-27 19:29:03 +0300

    Time: 7 ms, Memory: 13.7 MB - LeetHub

--- 0add-binary/0add-binary.py
+++ 0add-binary/0add-binary.py
@@ -6,5 +6,30 @@ class Solution(object):
         :rtype: str
         """
         # The first solution. Doesn't need any knowledge about loops and strings.
+        """
         ret = int(a,2) + int(b,2)
         return('{0:b}'.format(ret))
+        """
+        # The second.
+        ret = str()
+        tmp = 0
+        ptr_a, ptr_b = len(a) - 1, len(b) - 1
+        while ptr_a >= 0 or ptr_b >= 0 or tmp > 0:
+            if ptr_a >= 0:
+                tmp = tmp + int(a[ptr_a])
+            if ptr_b >= 0:
+                tmp = tmp + int(b[ptr_b])
+            if tmp == 0:
+                ret = ret + '0'
+            elif tmp == 1:
+                ret = ret + '1'
+                tmp = 0
+            elif tmp == 2:
+                ret = ret + '0'
+                tmp = 1
+            else: # tmp > 2
+                ret = ret + '1'
+                tmp = 1
+            ptr_a -= 1
+            ptr_b -= 1
+        return(ret[::-1])
