c2eada1086974e725eca530dab919f350df95efa
Alexey <alexeyantropov@gmail.com>
2023-02-27 16:37:55 +0300

    Time: 15 ms, Memory: 13.5 MB - LeetHub

--- spiral-matrix/spiral-matrix.py
+++ spiral-matrix/spiral-matrix.py
@@ -7,6 +7,8 @@ class Solution(object):
         m = len(matrix)
         n = len(matrix[0])
         # A simple solution, it just follows the Hint #1.
+        # It needs extra memory only for storing the answer.
+        """
         x_start = 0
         y_start = 0
         x_max = n-1
@@ -27,5 +29,17 @@ class Solution(object):
             x_start += 1
         # A cheet code for fixing doubles in the 'ret' list for non-squared matrixes.
         return(ret[:m*n])
-        
+        """
+        # A next try. A main loop will remove elements that it will return.
+        # Note: the source matrix will be changed after the main loop!
+        ret = []
+        while len(matrix) > 0:
+            ret = ret + matrix.pop(0)
+            if len(matrix) == 0 or len(matrix[0]) == 0 : break
+            ret = ret + [x.pop() for x in matrix]
+            if len(matrix) == 0: break
+            ret = ret + matrix.pop()[::-1]
+            if len(matrix) == 0 or len(matrix[0]) == 0: break
+            ret = ret + [x.pop(0) for x in matrix][::-1]
+        return(ret)
                     
