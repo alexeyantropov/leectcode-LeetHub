82b4a5812e11646f3f78019098ce32f450fc5148
Alexey <alexeyantropov@gmail.com>
2023-02-28 21:54:12 +0300

    Time: 21 ms, Memory: 13.5 MB - LeetHub

--- 0add-binary/0add-binary.py
+++ 0add-binary/0add-binary.py
@@ -11,6 +11,7 @@ class Solution(object):
         return('{0:b}'.format(ret))
         """
         # The second.
+        """
         ret = str()
         tmp = 0
         ptr_a, ptr_b = len(a) - 1, len(b) - 1
@@ -32,4 +33,21 @@ class Solution(object):
                 tmp = 1
             ptr_a -= 1
             ptr_b -= 1
+        return(ret[::-1])
+        """
+        # The third one, more clear.
+        ret, tmp, ptr_a, ptr_b = '', 0, len(a)-1, len(b)-1 
+        while ptr_a >= 0 or ptr_b >= 0:
+            if ptr_a >= 0:
+                tmp = tmp + int(a[ptr_a])
+            if ptr_b >= 0: 
+                tmp = tmp + int(b[ptr_b])
+            ret = ret + str(tmp % 2)
+            if tmp > 1:
+                tmp = 1
+            else:
+                tmp = 0
+            ptr_a -= 1; ptr_b -= 1
+        if tmp > 0:
+            ret = ret + str(tmp)
         return(ret[::-1])
