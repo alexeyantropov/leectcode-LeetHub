eebb04600934e35ff33b4db6a5a9c664bce1e334
Alexey <alexeyantropov@gmail.com>
2023-03-03 18:31:23 +0300

    Time: 286 ms, Memory: 13.7 MB - LeetHub

--- 0max-consecutive-ones/0max-consecutive-ones.py
+++ 0max-consecutive-ones/0max-consecutive-ones.py
@@ -4,6 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # [0,0,1,1,0,1,1,1]
+        # The first sol, with counters.
+        """
         tmp = 0
         ret = 0
         for i in range(len(nums)):
@@ -12,4 +15,16 @@ class Solution(object):
             ret = max(ret, tmp)
             if nums[i] == 0:
                 tmp = 0
+        return(ret)
+        """
+        # The second one, with two pointes technique
+        slow, fast = 0, 0
+        ret = 0
+        while fast < len(nums):
+            if nums[fast] == 1:
+                fast += 1
+            else:
+                fast += 1
+                slow = fast
+            ret = max(ret, fast - slow)
         return(ret)
