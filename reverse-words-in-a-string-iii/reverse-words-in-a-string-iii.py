a499fff45b38f9de421075a1635dc52719730972
Alexey <alexeyantropov@gmail.com>
2023-03-08 19:19:25 +0300

    Time: 2222 ms, Memory: 15 MB - LeetHub

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -11,6 +11,7 @@ class Solution(object):
         # The second one. Pointers are in the game.
         # Let the 's' string size is a constant!
         # In the other way we could add an extra space in the end of the string to make an edge condition for the last symbol easier.
+        """
         l, r, ret = 0, 0, str()
         while r < len(s):
             if s[r] == ' ' or r == len(s) - 1:
@@ -28,3 +29,19 @@ class Solution(object):
             else:
                 r += 1
         return(ret)
+        """
+        # Next one. The 's' string is changeable. A little faster than the second solution.
+        s += ' '
+        l, r, original_len = 0, 0, len(s)
+        while r < original_len:
+            if s[r] == ' ':
+                l_, r_ = l, r - 1
+                while l_ <= r_:
+                    s += s[r_]
+                    r_ -= 1
+                s += ' '
+                r += 1; l = r
+            else:
+                r += 1
+        return(s[original_len:len(s)-1])
+        
