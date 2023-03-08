40f9130f3e006fb2b87a66395d0e6f2cc529248b
Alexey <alexeyantropov@gmail.com>
2023-03-08 18:58:11 +0300

    Time: 701 ms, Memory: 14.2 MB - LeetHub

--- reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
+++ reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.py
@@ -5,4 +5,26 @@ class Solution(object):
         :rtype: str
         """
         # The first solution there are built-in methods.
+        """
         return(' '.join([x[::-1] for x in s.split()]))
+        """
+        # The second one. Pointers are in the game.
+        # Let the 's' string size is a constant!
+        # In the other way we could add an extra space in the end of the string to make an edge condition for the last symbol easier.
+        l, r, ret = 0, 0, str()
+        while r < len(s):
+            if s[r] == ' ' or r == len(s) - 1:
+                if r == len(s) - 1: # there is the edge condition for a last string.
+                    r_ = r
+                else:
+                    r_ = r - 1
+                l_ = l
+                if len(ret) > 0:   
+                    ret = ret + ' '
+                while l_ <= r_:
+                    ret = ret + s[r_]
+                    r_ -= 1
+                r += 1; l = r
+            else:
+                r += 1
+        return(ret)
