34220f8c850c5c1e46a25b9eeccd4395f0c0917c
Alexey <alexeyantropov@gmail.com>
2023-03-08 13:07:03 +0300

    Time: 31 ms, Memory: 13.9 MB - LeetHub

--- reverse-words-in-a-string/reverse-words-in-a-string.py
+++ reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -5,5 +5,20 @@ class Solution(object):
         :rtype: str
         """
         # The first way, maximum built-in functions. What could be better?
+        '''
         return(' '.join(s.split()[::-1]))
-        
+        '''
+        # The second one.
+        initial_size, l, r = len(s), len(s) - 1, len(s)
+        while l > -1:
+            if s[l] == ' ' and l == r - 1:
+                l -= 1; r -= 1
+                continue
+            if s[l] != ' ' and l > 0:
+                l -= 1
+            else:
+                if s[l] != ' ':
+                    s = s + ' '
+                s = s + s[l:r]
+                l -= 1; r = l + 1
+        return(s[initial_size + 1:len(s)])
