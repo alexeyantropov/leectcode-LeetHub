c3b923cac4723cafcff99ad10ac1628d76cc4e16
Alexey <alexeyantropov@gmail.com>
2023-03-09 13:49:06 +0300

    Time: 286 ms, Memory: 39 MB - LeetHub

---
+++ design-hashset/design-hashset.py
@@ -0,0 +1,16 @@
+class MyHashSet(object):
+
+    # The first (fast) solution. It needs O(max key value) memory because of internal storage for data. Better solution is linked lists I would like to do it later.     
+    def __init__(self):
+        max_key = 10**6 + 1
+        self.storage = [False]*max_key
+        
+    def add(self, key):
+        if not self.storage[key]:
+            self.storage[key] = True
+
+    def remove(self, key):
+        self.storage[key] = False
+        
+    def contains(self, key):
+        return(self.storage[key])
