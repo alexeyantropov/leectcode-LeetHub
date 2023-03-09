6de214d8fc9cae05f6f8674de50c4fe11cf1deca
Alexey <alexeyantropov@gmail.com>
2023-03-09 17:19:50 +0300

    Time: 407 ms, Memory: 23.7 MB - LeetHub

--- design-hashmap/design-hashmap.py
+++ design-hashmap/design-hashmap.py
@@ -1,22 +1,40 @@
 class MyHashMap(object):
 
     # The simplest idea is a list for low-level storage.
-    # Needs O(max key) memory and works with O(1) complexity for standard methods.
-    # If keys is alphanumeric then it will need O(2 * max key) memory for storring keys and values.
-    
-    # The next way is using a tree of lists, it would need less amount of memory.
+    # Needs O(max key) memory and works with O(1) complexity for standard methods.    
+    # The next way is using a lot of lists (buckets) and simple hash func, it would need less amount of memory.
     
     def __init__(self):
-        self.storage = [None] * (10**6 + 1)
+        self.max_key = 10**6
+        self.buckets = 100
+        self.storage = [None] * (self.buckets + 1)
+        
+    def get_bucket(self, key):
+        return(key % self.buckets)
+    
+    def get_offset(self, key):
+        return(key // self.buckets)
         
     def put(self, key, value):
-        self.storage[key] = value
+        bucket = self.get_bucket(key)
+        offset = self.get_offset(key)
+        if self.storage[bucket] == None:
+            self.storage[bucket] = [None] * ((self.max_key // self.buckets) + 1)
+        self.storage[bucket][offset] = value
         
     def get(self, key):
-        if self.storage[key] == None:
+        b = self.get_bucket(key)
+        o = self.get_offset(key)
+        if self.storage[b] == None:
+            return(-1)
+        if self.storage[b][o] == None:
             return(-1)
         else:
-            return(self.storage[key])
+            return(self.storage[b][o])
         
     def remove(self, key):
-        self.storage[key] = None        
+        b = self.get_bucket(key)
+        o = self.get_offset(key)
+        print(key, b, o)
+        if not self.storage[b] == None:
+            self.storage[b][o] = None
