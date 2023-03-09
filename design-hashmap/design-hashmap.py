91def7c03a742f60a4e03d26e45e729e8a0bf3ad
Alexey <alexeyantropov@gmail.com>
2023-03-09 16:07:35 +0300

    Time: 394 ms, Memory: 38.7 MB - LeetHub

--- design-hashmap/design-hashmap.py
+++ design-hashmap/design-hashmap.py
@@ -2,6 +2,7 @@ class MyHashMap(object):
 
     # The simplest idea with an a list for low-level storage.
     # Needs O(max key) memory and works with O(1) complexity for standard methods.
+    # If keys will be alphanumeric then it needs O(2 * max key) memory for storring keys and values.  
     
     def __init__(self):
         self.storage = [None] * (10**6 + 1)
