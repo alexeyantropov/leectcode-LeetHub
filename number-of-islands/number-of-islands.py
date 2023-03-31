76666bf8deabe6943e8869c34ac37004018c1f26
Alexey <alexeyantropov@gmail.com>
2023-03-31 16:30:25 +0300

    Time: 348 ms, Memory: 32.3 MB - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -5,34 +5,29 @@ class Solution(object):
         :rtype: int
         """
         # There is a solution based on articles 'Queue and BFS' & 'BFS - Template'.
-        # Main idea is inqueing and dequeing suitable elements from the grid into queue.
+        # Main idea is enqueing and dequeing suitable elements from the grid into queue.
         # If an element is a land and the element isn't visited before then
         # we coud run a loop (dequeue) and check neighbour elememts (inqueue or skip
         # by the same rule as for the element).
         # Time compexity is O(n*m), the algo should check all elements.
         # space comp. is O(n*m), for the 'visited' list. 
         
-        # Initial variables and buffers.
         m, n = len(grid), len(grid[0])
         queue, ret, visited = list(), 0, set()
-        # A helper for build neighobur elements list.
-        neighoburs_addrs = [[-1,0], [+1,0], [0,-1], [0, +1]]
+        neighoburs_addrs = [[-1,0], [+1,0], [0,-1], [0, +1]] # For building neighobur elements list.
         for i in range(m):
             for j in range(n):
-                # Check a element: land/water and is/isn't visited.
                 if grid[i][j] == '0' or (i,j) in visited:
                     continue
-                # If the element is not visited before country then start BFS.
-                visited.add((i,j))
+                visited.add((i,j)) # BFD is started here!
                 queue.append([i,j])
                 while len(queue) > 0:
-                    point = queue.pop(0) # dequeue an element
+                    point = queue.pop(0) # Dequeue an element.
                     for neighobur_addr in neighoburs_addrs:
                         [h,v] = [x+y for x,y in zip(point, neighobur_addr)]
                         if 0 <= h < m and 0 <= v < n and (h,v) not in visited and grid[h][v] == '1':
                             queue.append([h,v])
-                            visited.add((h,v)) # inqueue new suitable elements
-                # All elements have been checked, it can increment a counter.
+                            visited.add((h,v)) # Enqueue new suitable elements.
                 ret += 1
         return(ret)
             
