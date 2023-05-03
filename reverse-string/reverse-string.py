0bac5b620ddb209f6d9c7d2e4129c3b325abe47b
Alexey <alexeyantropov@gmail.com>
2023-05-03 15:19:24 +0300

    Time: 170 ms, Memory: 43.4 MB - LeetHub

--- reverse-string/reverse-string.py
+++ reverse-string/reverse-string.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def reverseString(self, s):
+    def reverseString(self, s, l = None, r = None):
         """
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
@@ -8,9 +8,20 @@ class Solution(object):
         """
         s = s[::-1]
         """
-        # The expected sol with two-pointers technique.
+        # The expected sol with the two-pointers technique.
+        """
         l, r = 0, len(s)-1
         while l < r:
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
+        """
+        # The third sol with the recursion approach.
+        # For this method I changed args of the function to avoid add an extra subfunction.
+        if l == None or r == None:
+            l, r = 0, len(s) - 1
+        if not l < r:
+            return()
+        s[l], s[r] = s[r], s[l]
+        self.reverseString(s, l+1, r-1)
+        
